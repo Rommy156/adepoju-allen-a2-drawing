75a5dea1d74460b54fac7d63836ac4f5e2bb4556
romilolaadepoju@gmail.com <romilolaadepoju@gmail.com>
2024-10-05 21:43:07 -0400

    delare spin position

--- adepoju-allen-a2-drawing(Update)/Game.cs
+++ adepoju-allen-a2-drawing(Update)/Game.cs
@@ -53,6 +53,22 @@ namespace Game10003
             Draw.FillColor = Color.OffWhite;
             Draw.LineColor = Color.Black;
 
+            //lets declare  wheel1
+
+                            Vector2 wheel1Pos = new Vector2(200, 300);
+                            Vector2 radiusLine = new Vector2(1, 0);
+                            radiusLine.X = 10f * (float)Math.Cos(Time.SecondsElapsed);
+                            radiusLine.Y = 10f * (float)Math.Sin(Time.SecondsElapsed);
+
+                            //lets declare wheel2
+                            Vector2[] line = new Vector2[2];
+                            Vector2 vector2 = new Vector2();
+
+                            Vector2 wheel2Pos = new Vector2(300, 300);
+                            Vector2 radiusLine1 = new Vector2(1, 0);
+                            radiusLine1.X = 10f * (float)Math.Cos(Time.SecondsElapsed);
+                            radiusLine1.Y = 10f * (float)Math.Sin(Time.SecondsElapsed);
+
 
 
 
